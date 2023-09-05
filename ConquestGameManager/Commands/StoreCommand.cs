3e18ed879e46ca4a0e1621fdeb6bab3cf39d94e1
Mikey <michael.knox@talktalk.net>
2023-09-05 23:00:08 +0100

    Added a /store command

--- ConquestGameManager/Commands/StoreCommand.cs
+++ ConquestGameManager/Commands/StoreCommand.cs
@@ -4,20 +4,18 @@ using Rocket.Unturned.Player;
 
 namespace ConquestGameManager.Commands
 {
-    internal class DiscordCommand : IRocketCommand
+    internal class StoreCommand : IRocketCommand
     {
         public AllowedCaller AllowedCaller => AllowedCaller.Player;
-        public string Name => "discord";
-        public string Help => "Command to get a link to the discord";
-        public string Syntax => "/discord";
+        public string Name => "store";
+        public string Help => "Command to get a link to the store";
+        public string Syntax => "/store";
         public List<string> Aliases => new();
         public List<string> Permissions => new();
 
         public void Execute(IRocketPlayer caller, string[] command)
         {
-            if (caller is UnturnedPlayer player) Utility.OpenUrl(player, "Join Our Discord!", "https://discord.gg/kxrkhaNd4T");
+            if (caller is UnturnedPlayer player) Utility.OpenUrl(player, "Our store!", "https://nordicroleplay.tebex.io/category/military-roleplay");
         }
     }
 }
-
-
