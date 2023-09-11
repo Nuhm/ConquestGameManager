4c4b8ba52c87a0f04751ab269441113211798d65
Mikey <michael.knox@talktalk.net>
2023-09-11 01:32:05 +0100

    Added /help command

---
+++ ConquestGameManager/Commands/HelpCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Rocket.API;
+using Rocket.Unturned.Player;
+
+namespace ConquestGameManager.Commands
+{
+    internal class HelpCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "help";
+        public string Help => "Command to get a link to help documents";
+        public string Syntax => "/help";
+        public List<string> Aliases => new();
+        public List<string> Permissions => new();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (caller is UnturnedPlayer player) Utility.OpenUrl(player, "For Help Join Our Discord!", "https://discord.gg/kxrkhaNd4T");
+        }
+    }
+}
